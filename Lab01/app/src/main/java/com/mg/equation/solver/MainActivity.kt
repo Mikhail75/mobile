4ca2c1cf2d398a0ef387c595f4e17dd812152bac
Mikhail <glizerin@gmail.com>
2019-02-18 23:31:01 +0300

    [Lab01] [EquationSolver] Сверстал часть вертикального макета

--- Lab01/app/src/main/java/com/mg/equation/solver/MainActivity.kt
+++ Lab01/app/src/main/java/com/mg/equation/solver/MainActivity.kt
@@ -2,11 +2,15 @@ package com.mg.equation.solver
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView3.text = Html.fromHtml("A*x<sup><small>2</small></sup> + B * x + C = 0")
     }
 }
