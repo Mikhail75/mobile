ef8a7970315ac244d42cdd266f1629a9b7be9851
Mikhail <glizerin@gmail.com>
2019-02-21 23:03:39 +0300

    [Lab01] [EquationSolver] Реализовал обработку данных, введённых пользователем, и вывод решения уравнения

--- Lab01/app/src/main/java/com/mg/equation/solver/MainActivity.kt
+++ Lab01/app/src/main/java/com/mg/equation/solver/MainActivity.kt
@@ -5,8 +5,16 @@ import android.os.Bundle
 import android.text.Html
 import android.widget.TextView
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private data class Factors (
+        var a: Double?,
+        var b: Double?,
+        var c: Double?
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +25,57 @@ class MainActivity : AppCompatActivity() {
 
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener{
-            val app = CustomApp.getApplication()
-            textView.text = app.test()
+            onSolveButtonClick()
         }
     }
 
+    private fun onSolveButtonClick() {
+        val factors = getFactors()
+        if (!validateFactors(factors))
+        {
+            return
+        }
 
+        val result = solve(factors.a!!, factors.b!!, factors.c!!)
+
+        var resultText = "D: " + result.d.toString()
+        if (result.d >= 0.0)
+        {
+            resultText += "\nX1: " + result.x1!!.toString()
+            if (result.d > 0.0)
+            {
+                resultText += "\nX2: " + result.x2!!.toString()
+            }
+        }
+
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+        resultTextView.text = resultText
+    }
+
+    private fun getFactors() : Factors {
+        val textA = findViewById<EditText>(R.id.inputA).text
+        val textB = findViewById<EditText>(R.id.inputB).text
+        val textC = findViewById<EditText>(R.id.inputC).text
+
+        return Factors(
+            textA.toString().toDoubleOrNull(),
+            textB.toString().toDoubleOrNull(),
+            textC.toString().toDoubleOrNull()
+        )
+    }
+
+    private fun validateFactors(factors: Factors) : Boolean {
+        if (factors.a == null || factors.b == null || factors.c == null )
+        {
+            // TODO tost
+            return false
+        }
+        if (factors.a!! == 0.0)
+        {
+            Toast.makeText(this@MainActivity, "The factor A cannot be equal to 0", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        return true
+    }
 }
