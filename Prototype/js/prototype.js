d2ad457030fe7b5759faa2436695672b2c84ccc4
Mikhail <glizerin@gmail.com>
2019-04-13 12:17:42 +0300

    [Prototype] Переключение тональностей, звук

--- Prototype/js/prototype.js
+++ Prototype/js/prototype.js
@@ -21,24 +21,52 @@ class Hotspot {
 const screen = document.getElementsByClassName("screen")[0];
 /** @type {!Map<!Hotspot, function()>} */
 const hotspotHandlers = new Map();
-/** @type {!Array<!Element>} */
-const hotspotTooltips = [];
+let soundIsPlaying = false;
 
 function showMainScreen() {
 	screen.style.background = `no-repeat center url("./image/main_screen_1.svg")`;
 	hotspotHandlers.clear();
-	hotspotHandlers.set(new Hotspot(160, 100, 60, 60), () => {
+	hotspotHandlers.set(new Hotspot(88, 29, 60, 60), () => {
 		showMainScreen2();
 	});
-
+	hotspotHandlers.set(new Hotspot(196, 115, 167, 40), () => {
+		showMainScreen3();
+	});
+	hotspotHandlers.set(new Hotspot(19, 344, 167, 60), () => {
+		playSound("./sound/sound1.wav");
+	});
+	hotspotHandlers.set(new Hotspot(196, 485, 167, 60), () => {
+		playSound("./sound/sound2.wav");
+	});
 }
 
 function showMainScreen2() {
 	screen.style.background = `no-repeat center url("./image/main_screen_2.svg")`;
 	hotspotHandlers.clear();
-	hotspotHandlers.set(new Hotspot(89, 100, 60, 60), () => {
+	hotspotHandlers.set(new Hotspot(160, 29, 60, 60), () => {
 		showMainScreen();
 	});
+}
+
+function showMainScreen3() {
+	screen.style.background = `no-repeat center url("./image/main_screen_3.svg")`;
+	hotspotHandlers.clear();
+	hotspotHandlers.set(new Hotspot(19, 115, 167, 40), () => {
+		showMainScreen();
+	});
+}
+
+/**
+ * @param {string} source
+ */
+function playSound(source) {
+	if (!soundIsPlaying)
+	{
+		soundIsPlaying = true;
+		const audio = new Audio(source);
+		audio.onended = () => soundIsPlaying = false;
+		audio.play();
+	}
 }
 
 /**
